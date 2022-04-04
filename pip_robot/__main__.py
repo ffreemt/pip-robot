23e0a52f783ce423da5eaa55636d784450b6f5b6
freemt <yucongo+fmt@gmail.com>
2022-04-04 19:37:01 +0800

    Bump to 0.1.0a2

--- pip_robot/__main__.py
+++ pip_robot/__main__.py
@@ -51,6 +51,18 @@ def _version_callback(value: bool) -> None:
 
 @app.command()
 def main(
+    command: List[str] = typer.Argument(
+        ...,
+        metavar="command [args or options]...",
+        help="""a command to check against missing pyi packages, typically python foo.py or "python -m bar" (note the quotes) """,
+    ),
+    debug: bool = typer.Option(
+        False,
+        "--debug",
+        "-d",
+        is_flag=True,
+        help="Turn on debug mode. Note that debug can also be turned by set LOGLEVEL=debug or export LOGLEVEL=debug.",
+    ),
     version: Optional[bool] = typer.Option(  # pylint: disable=(unused-argument
         None,
         "--version",
@@ -69,12 +81,25 @@ def main(
 
     or pythom -m pip_robot python foo.py
 
-    or pip-robot python -m bar
+    or pip-robot "python -m bar"  # (must enclosed in quotes
+    if options (- or/and --) are present in the command to test.)
 
     """
+    if debug:
+        logger.info("debug is on")
+        # logzero.loglevel(loglevel(10, force=True))
+        # logzero.loglevel(loglevel(10))
+        # logzero.loglevel(loglevel(20, force=True))
+
+    logger.debug("debug")
+
+    typer.echo(f"Your command: {' '.join(command)}")
+    logger.info("Nothing yet... we are working on it.")
+    # raise NotImplementedError("Coming soon...")
 
 
 if __name__ == "__main__":
     # set LOGLEVEL=10 or debug or DEBUG to turn on debug
-    logger.debug(" debug on")
+    logger.debug(" debug on (main)")
     app()
+    logger.debug("wont show debug on (main) 1")
